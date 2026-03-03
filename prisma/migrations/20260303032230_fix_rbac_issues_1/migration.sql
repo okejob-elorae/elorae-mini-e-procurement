42887ee8270ac4b0ce06937e09fcc8abbb3b8f03
JuicyBurger <johananpoerwanto@gmail.com>
2026-03-03 11:47:53 +0800

    try fix migration logical order error on prod

--- prisma/migrations/20260303032230_fix_rbac_issues_1/migration.sql
+++ prisma/migrations/20260303032230_fix_rbac_issues_1/migration.sql
@@ -1,17 +1,11 @@
--- DropForeignKey
-ALTER TABLE `rejectedgoodsledger` DROP FOREIGN KEY `RejectedGoodsLedger_itemId_fkey`;
+-- Only drop RBAC-related FKs so the next migration can add them back
+-- (after creating RoleDefinition/Permission). Do NOT drop unrelated FKs
+-- (RejectedGoodsLedger_itemId, Supplier_approvedById, WorkOrder_poId) or
+-- referential integrity is lost and schema would be out of sync.
 
--- DropForeignKey
+-- DropForeignKey (RolePermission references RoleDefinition and Permission)
 ALTER TABLE `rolepermission` DROP FOREIGN KEY `RolePermission_permissionId_fkey`;
-
--- DropForeignKey
 ALTER TABLE `rolepermission` DROP FOREIGN KEY `RolePermission_roleId_fkey`;
 
--- DropForeignKey
-ALTER TABLE `supplier` DROP FOREIGN KEY `Supplier_approvedById_fkey`;
-
--- DropForeignKey
+-- DropForeignKey (User.roleId will be re-added in add_rbac migration)
 ALTER TABLE `user` DROP FOREIGN KEY `User_roleId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `workorder` DROP FOREIGN KEY `WorkOrder_poId_fkey`;
