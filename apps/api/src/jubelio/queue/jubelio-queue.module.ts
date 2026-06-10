36bee0e761a2634ffbde895345561b4e0bc86309
RifkyLTF <rifky.luthfan@qiscus.com>
2026-06-10 21:23:48 +0700

    feat(api): route salesorder + salesreturn + product webhook events

--- apps/api/src/jubelio/queue/jubelio-queue.module.ts
+++ apps/api/src/jubelio/queue/jubelio-queue.module.ts
@@ -2,17 +2,22 @@ import { Module } from "@nestjs/common";
 import { BullModule } from "@nestjs/bullmq";
 import { AdminModule } from "../../admin/admin.module";
 import { PrismaModule } from "../../db/prisma.module";
+import { JubelioCatalogModule } from "../catalog/catalog.module";
 import { JUBELIO_WEBHOOK_QUEUE } from "./jubelio-queue.config";
 import { WebhookQueueService } from "./webhook-queue.service";
 import { WebhookProcessor } from "./webhook-processor.service";
 import { JubelioEventRouter } from "./event-router";
 import { StockWebhookHandler } from "../handlers/stock.handler";
+import { SalesOrderWebhookHandler } from "../handlers/salesorder.handler";
+import { SalesReturnWebhookHandler } from "../handlers/salesreturn.handler";
+import { ProductWebhookHandler } from "../handlers/product.handler";
 import { UnhandledEventHandler } from "../handlers/unhandled.handler";
 
 @Module({
   imports: [
     PrismaModule,
     AdminModule,
+    JubelioCatalogModule,
     BullModule.registerQueue({ name: JUBELIO_WEBHOOK_QUEUE }),
   ],
   providers: [
@@ -20,6 +25,9 @@ import { UnhandledEventHandler } from "../handlers/unhandled.handler";
     WebhookProcessor,
     JubelioEventRouter,
     StockWebhookHandler,
+    SalesOrderWebhookHandler,
+    SalesReturnWebhookHandler,
+    ProductWebhookHandler,
     UnhandledEventHandler,
   ],
   exports: [WebhookQueueService],
