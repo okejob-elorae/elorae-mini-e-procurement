be073e039821d173b7146d4082fdacb8fd45d2b3
RifkyLTF <rifky.luthfan@qiscus.com>
2026-06-28 22:26:53 +0700

    feat(api): register JubelioImageUploadService in jubelio module

--- apps/api/src/jubelio/jubelio.module.ts
+++ apps/api/src/jubelio/jubelio.module.ts
@@ -4,13 +4,14 @@ import { JubelioController } from "./jubelio.controller";
 import { JubelioHttpService } from "./http.service";
 import { JubelioTokenService } from "./token.service";
 import { JubelioApiCallLogger } from "./api-call-logger.service";
+import { JubelioImageUploadService } from "./image-upload.service";
 import { JubelioQueueModule } from "./queue/jubelio-queue.module";
 import { ReturnsModule } from "./returns/returns.module";
 
 @Module({
   imports: [JubelioQueueModule, forwardRef(() => ReturnsModule)],
   controllers: [JubelioController],
-  providers: [JubelioConfig, JubelioTokenService, JubelioHttpService, JubelioApiCallLogger],
-  exports: [JubelioConfig, JubelioTokenService, JubelioHttpService],
+  providers: [JubelioConfig, JubelioTokenService, JubelioHttpService, JubelioApiCallLogger, JubelioImageUploadService],
+  exports: [JubelioConfig, JubelioTokenService, JubelioHttpService, JubelioImageUploadService],
 })
 export class JubelioModule {}
